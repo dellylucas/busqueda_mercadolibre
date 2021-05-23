7f340170acd798ceed4d14a9660ba26f17f2bd3d
dellylucas <dellylucas@hotmail.com>
2021-05-23 00:56:49 -0500

    creacion vistas y navegacion

--- app/src/main/java/com/dfl/busquedamercadolibre/view/ui/MainActivity.kt
+++ app/src/main/java/com/dfl/busquedamercadolibre/view/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.dfl.busquedamercadolibre.view.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.dfl.busquedamercadolibre.R
 
 class MainActivity : AppCompatActivity() {
